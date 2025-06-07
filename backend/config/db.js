f15a300b9e515665d89c2e333d026675c9ccd288
Saloomaa <salmazah07@gmail.com>
2025-06-08 00:21:15 +0100

    fix le lien de database dans le fichier db.js pour utiliser railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,15 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // nécessaire pour Railway et autres hébergements
+  },
+  //user: process.env.DB_USER,
+  //password: process.env.DB_PASSWORD,
+  //host: process.env.DB_HOST,
+  //port: process.env.DB_PORT,
+  //database: process.env.DB_NAME,
 });
 
 // Testez la connexion
